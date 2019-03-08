7e92679f374d244dfb48648e6ef64925c64f72d1
aaamir829 <aaamir829@gmail.com>
2019-03-08 19:25:39 +0330

    two new videos

--- Contactsproj/urls.py
+++ Contactsproj/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url,include
 from app1 import views
 
 urlpatterns = [
-    
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from app1.models import PhonesTable,ContactsTable
+
+
+
+
+def index(request):
+    phones=PhonesTable.objects.all()
+    return render(request, 'app1/index.html', context={'ph':phones})
