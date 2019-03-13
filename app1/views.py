58bc75687753113ed4f970bbe5fe47c5d3215c7b
omidhamidi61 <omidhamidi61@yahoo.com>
2019-03-13 22:02:54 +0330

    HME TASK

--- app1/views.py
+++ app1/views.py
@@ -7,3 +7,9 @@ from app1.models import PhonesTable,ContactsTable
 def index(request):
     phones=PhonesTable.objects.all()
     return render(request, 'app1/index.html', context={'ph':phones})
+
+<<<BY HME>>>
+
+def function(request):
+    names = ContactsTable.objects.all()
+    return render(request, 'app1/index.html', context={'nm':names + familys})
